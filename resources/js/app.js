4ee1b609bfec11a1b7ccc298ac9da78a34a20269
Victor Santana <57636782+victorsb2930@users.noreply.github.com>
2025-11-04 01:48:52 +0100

    Fix js chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -33,7 +33,7 @@ const pageModuleMap = {
 	,'user-appointments': './pages/user.appointments.js'
 	,'professionals-search': './pages/professionals.search.js'
 	,'plans': './components/plans.js'
-	,'chat': './pages/messages.js'
+	,'chat': './pages/chat.js'
 };
 
 async function initPage() {
